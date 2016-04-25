7fefe31b2a5c9082c362c0ebe448e47d47b085fe
Bathory Alex <bathory.alex@gmail.com>
2016-04-25 13:06:17 +0200

    Threading, Exercise 1-2-3

---
+++ Threading/Exercise 2/MutexToCreateSingleInstApp/MutexToCreateSingleInstApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Mutex oneMutex = null;
+        const string MutexName = "RUNMEONLYONCE";
+
+        try 
+        {
+            oneMutex = Mutex.OpenExisting(MutexName);
+        }
+        catch (WaitHandleCannotBeOpenedException)
+        {
+            // Cannot open the mutex because it doesn't exist 
+        }
+
+         
+        if (oneMutex == null)
+        {
+            oneMutex = new Mutex(true, MutexName);
+        }
+        else
+        {
+            oneMutex.Close();
+            return;
+        }
+
+        Console.WriteLine("Our Application");
+        Console.Read();
+    }
+}
